1de8330cf5b4ec5ccf807f9395f3f07762c20320
sun <363599@gmail.com>
2020-01-14 12:44:32 +0800

    feat: add #101 breadth_first_search#SymmetricTree

---
+++ src/breadth_first_search/SymmetricTree.java
@@ -0,0 +1,80 @@
+package breadth_first_search;
+
+
+/**
+ *给定一个二叉树，检查它是否是镜像对称的。
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ * 说明:
+ * 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author sun. on 1/11/2020.
+ */
+public class SymmetricTree {
+	public boolean isSymmetric(TreeNode root) {
+		if (root == null) {
+			return true;
+		}
+		return isMirrorTree(root.left, root.right);
+	}
+
+	private boolean isMirrorTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		}
+		if (p == null || q == null) {
+			return false;
+		}
+		if (p.val != q.val) {
+			return false;
+		}
+		return isMirrorTree(p.left, q.right) && isMirrorTree(p.right, q.left);
+	}
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(1);
+		TreeNode a = new TreeNode(2);
+		TreeNode b = new TreeNode(2);
+
+		TreeNode c = new TreeNode(3);
+		TreeNode d = new TreeNode(4);
+
+		TreeNode e = new TreeNode(3);
+		TreeNode f = new TreeNode(4);
+
+		root.left = a;
+		root.right = b;
+
+		a.left = c;
+		a.right = d;
+
+		b.left = f;
+		b.right = e;
+		System.out.println(new SymmetricTree().isSymmetric(root));
+	}
+}
+
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
